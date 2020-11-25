18a7e39f4f2e818a9b7e439ea05ec4dfa48b3a1b
Geeth <geethkuldeep@gmail.com>
2020-11-25 10:13:19 +0530

    first commit

---
+++ lib/dimension.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+
+GlobalKey _keyblack1 = GlobalKey();
+GlobalKey _keyblack2 = GlobalKey();
+GlobalKey _keyblack3 = GlobalKey();
+GlobalKey _keyblack4 = GlobalKey();
+GlobalKey _keyblack5 = GlobalKey();
+GlobalKey _keyblack6 = GlobalKey();
+GlobalKey _keyblack7 = GlobalKey();
+GlobalKey _keywhite1 = GlobalKey();
+GlobalKey _keywhite2 = GlobalKey();
+GlobalKey _keywhite3 = GlobalKey();
+GlobalKey _keywhite4 = GlobalKey();
+GlobalKey _keywhite5 = GlobalKey();
+
+
+getSizes1() {
+  final RenderBox renderBoxRed = _keyblack1.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions1() {
+      final RenderBox renderBoxRed = _keyblack1.currentContext.findRenderObject();
+      final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+      print("POSITION of Red: $positionRed ");
+}
+getSizes2() {
+  final RenderBox renderBoxRed = _keyblack2.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions2() {
+  final RenderBox renderBoxRed = _keyblack2.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes3() {
+  final RenderBox renderBoxRed = _keyblack3.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions3() {
+  final RenderBox renderBoxRed = _keyblack3.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes4() {
+  final RenderBox renderBoxRed = _keyblack4.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions4() {
+  final RenderBox renderBoxRed = _keyblack4.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes5() {
+  final RenderBox renderBoxRed = _keyblack5.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions5() {
+  final RenderBox renderBoxRed = _keyblack5.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes6() {
+  final RenderBox renderBoxRed = _keyblack6.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions6() {
+  final RenderBox renderBoxRed = _keyblack6.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes7() {
+  final RenderBox renderBoxRed = _keyblack7.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions7() {
+  final RenderBox renderBoxRed = _keyblack7.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes8() {
+  final RenderBox renderBoxRed = _keywhite1.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions8() {
+  final RenderBox renderBoxRed = _keywhite1.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes9() {
+  final RenderBox renderBoxRed = _keywhite2.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions9() {
+  final RenderBox renderBoxRed = _keywhite2.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes10() {
+  final RenderBox renderBoxRed = _keywhite3.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions10() {
+  final RenderBox renderBoxRed = _keywhite3.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes11() {
+  final RenderBox renderBoxRed = _keywhite4.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions11() {
+  final RenderBox renderBoxRed = _keywhite4.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+getSizes12() {
+  final RenderBox renderBoxRed = _keywhite5.currentContext.findRenderObject();
+  final sizeRed = renderBoxRed.size;
+  print("SIZE of Red: $sizeRed");
+}
+getPositions12() {
+  final RenderBox renderBoxRed = _keywhite5.currentContext.findRenderObject();
+  final positionRed = renderBoxRed.localToGlobal(Offset.zero);
+  print("POSITION of Red: $positionRed ");
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import 'package:tonic/tonic.dart';
+import 'package:flutter/services.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+import 'dart:convert';
+import 'package:web_flutter_piano/dimension.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  GlobalKey _keyblack1 = GlobalKey();
+  GlobalKey _keyblack2 = GlobalKey();
+  GlobalKey _keyblack3 = GlobalKey();
+  GlobalKey _keyblack4 = GlobalKey();
+  GlobalKey _keyblack5 = GlobalKey();
+  GlobalKey _keyblack6 = GlobalKey();
+  GlobalKey _keyblack7 = GlobalKey();
+  GlobalKey _keywhite1 = GlobalKey();
+  GlobalKey _keywhite2 = GlobalKey();
+  GlobalKey _keywhite3 = GlobalKey();
+  GlobalKey _keywhite4 = GlobalKey();
+  GlobalKey _keywhite5 = GlobalKey();
+
+
+
+  double get keyWidth => 94 + (125 * _widthRatio);
+  double _widthRatio = 0.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'roboVITics',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+          appBar: AppBar(title: Text("VR Piano")),
+          drawer: Drawer(
+              child: SafeArea(
+                  child: ListView(children: <Widget>[
+                    Container(height: 20.0),
+                    ListTile(title: Text("Change Width")),
+                    Slider(
+                        activeColor: Colors.redAccent,
+                        inactiveColor: Colors.white,
+                        min: 0.0,
+                        max: 1.0,
+                        value: _widthRatio,
+                        onChanged: (double value) =>
+                            setState(() => _widthRatio = value)),
+                  ]))),
+
+          body: ListView.builder(
+            itemCount: 1,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (BuildContext context, int index) {
+              return SafeArea(
+                child: Stack(children: <Widget>[
+                  Row(mainAxisSize: MainAxisSize.min, children: <Widget>[
+                    _buildKey(1,false,_keyblack1),
+                    _buildKey(2,false,_keyblack2),
+                    _buildKey(3,false,_keyblack3),
+                    _buildKey(4,false,_keyblack4),
+                    _buildKey(5,false,_keyblack5),
+                    _buildKey(6,false,_keyblack6),
+                    _buildKey(7,false,_keyblack7),
+                  ]),
+                  Positioned(
+                      left: 0.0,
+                      right: 0.0,
+                      bottom: 100,
+                      top: 0.0,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(width: keyWidth * .5),
+                            _buildKey(1,true,_keywhite1),
+                            _buildKey(2,true,_keywhite2),
+                            Container(width: keyWidth),
+                            _buildKey(3,true,_keywhite3),
+                            _buildKey(4,true,_keywhite4),
+                            _buildKey(5,true,_keywhite5),
+                            Container(width: keyWidth * .5),
+                          ])),
+                ]),
+              );
+            },
+          ),
+      ),
+    );
+  }
+  MainSizeAndPositionState(){
+    getSizes1();
+    getPositions1();
+    getSizes2();
+    getPositions2();
+    getSizes3();
+    getPositions3();
+    getSizes4();
+    getPositions4();
+    getSizes5();
+    getPositions5();
+    getSizes6();
+    getPositions6();
+    getSizes7();
+    getPositions7();
+    getSizes8();
+    getPositions8();
+    getSizes9();
+    getPositions9();
+    getSizes10();
+    getPositions10();
+    getSizes11();
+    getPositions11();
+    getSizes12();
+    getPositions12();
+  }
+
+  double posx = 100.0;
+  double posy = 100.0;
+
+
+
+  void playSound(int soundNumber) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+
+    assetsAudioPlayer.open(
+      Audio("note$soundNumber.wav"),
+    );
+  }
+
+  Widget _buildKey(int a, bool accidental,keynumber) {
+    final pianoKey = Stack(
+      children: <Widget>[
+        Semantics(
+            button: true,
+
+            child: Material(
+                borderRadius: borderRadius,
+                color: accidental ? Colors.black : Colors.white,
+                child: GestureDetector(
+                  child: InkWell(
+                    key: keynumber,
+                    borderRadius: borderRadius,
+                    highlightColor: Colors.grey,
+                    onTap: () {},
+                    onTapDown: (_) => playSound(a),
+                  ),
+
+                )
+            )
+        ),
+      ],
+    );
+    if (accidental) {
+      return Container(
+          width: keyWidth,
+          margin: EdgeInsets.symmetric(horizontal: 2.0),
+          padding: EdgeInsets.symmetric(horizontal: keyWidth * .1),
+          child: Material(
+              elevation: 6.0,
+              borderRadius: borderRadius,
+              shadowColor: Color(0x802196F3),
+              child: pianoKey));
+    }
+    return Container(
+        width: keyWidth,
+        child: pianoKey,
+        margin: EdgeInsets.symmetric(horizontal: 2.0));
+  }
+}
+const BorderRadiusGeometry borderRadius = BorderRadius.only(
+    bottomLeft: Radius.circular(10.0), bottomRight: Radius.circular(10.0));
